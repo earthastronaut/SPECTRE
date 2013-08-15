fc19c5226bf0167a1f2e58c41d67b63ea7d00954
astrodsg_laptop <gregersen.dylan@gmail.com>
2013-08-15 17:30:30 -0600

    Fixed the Setplt.f to work for text files. Mainly I had to get the wavout to be the correct value by linear interpolation between the two nearest pixels

--- Setplt.f
+++ Setplt.f
@@ -4,10 +4,28 @@ c*****this routine sets up the plot parameters for an individual line.
       include 'Dataval.com'
       include 'Plotval.com'
       include 'Widpar.com'
-      real*8 wavout
- 
+      real*8 wavout,m,b
+      integer ilower,iupper
+
       call findmin (real(ipt),x,npx,point,centint,emflag)
-      wavout = wave(point,npx,dispx)
+c     fix for dealing with text files
+      if (dispx(9) .eq. 0 .and. 
+     .     (dispx(1) .eq. 0 .or. dispx(2) .eq. 0 )) then
+c         wavout = wlx(ipt)
+c     interpolate for subpixel
+c        NOTE: int() rounds down
+         ilower = int(point)
+         iupper = int(point + 1.0)
+
+         if (ilower .le. 1) then ilower = 1
+         if (iupper .ge. npx) then iupper = npx
+         
+         m = (wlx(iupper)-wlx(ilower))/(real(iupper)-real(ilower))
+         b = wlx(ilower)-m*real(ilower)
+         wavout = m*point+b
+      else
+         wavout = wave(point,npx,dispx)
+      endif
       ileft = ipt - ndelt
       iright = ipt + ndelt
   
